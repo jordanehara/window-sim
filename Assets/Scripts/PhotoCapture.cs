c2ad87b2248eddea28edb165af096e722e09a906
Jordan Ehara <jordan.ehara@gmail.com>
2024-05-13 15:21:41 -0700

    clean up usings

--- Assets/Scripts/PhotoCapture.cs
+++ Assets/Scripts/PhotoCapture.cs
@@ -1,9 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.Android;
 using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
