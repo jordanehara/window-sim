fb23ceb9fc4fedb369320f1cf0729e1f81c64fbc
Jordan Ehara <jordan.ehara@gmail.com>
2024-05-13 15:16:30 -0700

    add photo capture

---
+++ Assets/Scripts/PhotoCapture.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("Photo taker")]
+    [SerializeField] private Image photoDisplayArea;
+
+    private const int height = 800, width = height;
+
+    private Texture2D screenCapture; // The photo we're capturing
+    
+    private void Start()
+    {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(CapturePhoto());
+        }
+    }
+
+    IEnumerator CapturePhoto()
+    {
+        yield return new WaitForEndOfFrame(); // make sure everything is rendered
+         
+        Rect regionToRead = new Rect(Screen.width/2-height/2, Screen.height/2-width/2, height, width); // area to "read pixels"
+
+        screenCapture.ReadPixels(regionToRead, 0, 0, false);
+        screenCapture.Apply(); // expensive function
+        ShowPhoto();
+    }
+
+    void ShowPhoto()
+    {
+        // create a sprite that will display the image
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, height, width), new Vector2(0.5f, 0.5f), 100.0f);
+
+        // Set where the photo area is going to be and spawn the sprite
+        photoDisplayArea.sprite = photoSprite;
+    }
+}
