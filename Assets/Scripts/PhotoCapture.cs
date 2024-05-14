df35b42f4320c94f86aa285a70ad155b27cb4953
Jordan Ehara <jordan.ehara@gmail.com>
2024-05-14 12:15:26 -0700

    add zoom feature

--- Assets/Scripts/PhotoCapture.cs
+++ Assets/Scripts/PhotoCapture.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.ComponentModel.Design;
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    #region Unity object input fields
+    [Header ("Camera")]
+    [SerializeField] private Camera camera;
+
     [Header("Photo taker")]
     [SerializeField] private Image photoDisplayArea;
     [SerializeField] private GameObject photoFrame;
@@ -12,14 +17,28 @@ public class NewBehaviourScript : MonoBehaviour
 
     [Header("Photo Fader Effect")]
     [SerializeField] private Animator fadingAnimation;
+    #endregion
 
+    #region Photo capture variables
     private const int height = 800, width = height;
     private Texture2D screenCapture; // The photo we're capturing
     private bool viewingPhoto;
-    
+    #endregion
+
+    #region Camera zoom variables
+    private float zoomLevel;
+    private float zoomMultiplier = 4f;
+    private float minZoomLevel = 2f;
+    private float maxZoomLevel = 5f;
+    private float velocity = 0f;
+    private float smoothTime = 0.1f;
+
+    #endregion
+
     private void Start()
     {
-        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        zoomLevel = camera.orthographicSize;
+        screenCapture = new Texture2D(width, height, TextureFormat.RGB24, false);
     }
 
     private void Update()
@@ -31,6 +50,19 @@ public class NewBehaviourScript : MonoBehaviour
             else
                 RemovePhoto();
         }
+        else
+        {
+            if (!viewingPhoto)
+                Zoom();
+        }
+    }
+
+    void Zoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        zoomLevel -= scroll * zoomMultiplier;
+        zoomLevel = Mathf.Clamp(zoomLevel, minZoomLevel, maxZoomLevel);
+        camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, zoomLevel, ref velocity, smoothTime);
     }
 
     IEnumerator CapturePhoto()
@@ -49,7 +81,7 @@ public class NewBehaviourScript : MonoBehaviour
 
     void ShowPhoto()
     {
-        // create a sprite that will display the image
+        // Create a sprite that will display the image
         Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, height, width), new Vector2(0.5f, 0.5f), 100.0f);
 
         // Set where the photo area is going to be and spawn the sprite
