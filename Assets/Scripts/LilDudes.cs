d628ea231a41716cac08c900d98d13a1826cca22
Jordan Ehara <jordan.ehara@gmail.com>
2024-06-02 17:34:27 -0700

    add the name of the lil dude above the sprite

--- Assets/Scripts/LilDudes.cs
+++ Assets/Scripts/LilDudes.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class LilDudes : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI spriteName;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,9 @@ public class LilDudes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // Move the lil dude name to above the lil dude
+        Vector3 spritePosition = this.gameObject.transform.position;
+        float spriteHeight = this.gameObject.GetComponent<RectTransform>().rect.height;
+        spriteName.transform.position = new Vector3(spritePosition.x, spritePosition.y + spriteHeight/2, spritePosition.z);
     }
 }
